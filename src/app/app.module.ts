f826d589321dd20ed6eacb8013e05dde0a976c93
amahmoud33 <amahmoud@uoguelph.ca>
2019-01-05 03:03:54 -0500

    - Completed intro for report - Completed about for report - Completed conclusion for report - Completed goals for report - Added footer component - More style tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CoopModule } from './coop/sf18/coop.module';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
     HomeComponent,
     AboutComponent,
     ContactComponent,
